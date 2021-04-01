9de640e145951eb2ccbc91098e70b941e4e752e6
GitHub Action <action@github.com>
2021-04-01 20:35:35 +0000

    Template cleanup

--- src/main/kotlin/com/github/nilsa/ideatestplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/nilsa/ideatestplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.nilsa.ideatestplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.nilsa.ideatestplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/nilsa/ideatestplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/nilsa/ideatestplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.nilsa.ideatestplugin.services
 
+import com.github.nilsa.ideatestplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
