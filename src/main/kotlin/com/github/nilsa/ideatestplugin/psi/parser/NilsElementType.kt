2890c85a196087cd85496ea9a68f46b7a488b44a
Nils Andresen <nils@nils-andresen.de>
2022-08-12 15:39:17 +0200

    moved all sources

--- src/main/kotlin/com/github/nilsa/ideatestplugin/psi/parser/NilsElementType.kt
+++
@@ -1,8 +0,0 @@
-package com.github.nilsa.ideatestplugin.psi.parser
-
-import com.github.nilsa.ideatestplugin.NilsLanguage
-import com.intellij.psi.tree.IElementType
-import org.jetbrains.annotations.NonNls
-
-class NilsElementType(@NonNls debugName: String) :
-    IElementType(debugName!!, NilsLanguage)
