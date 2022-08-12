fb552a5657626935275ad4ebc4a8cf2ea61d0157
Nils Andresen <nils@nils-andresen.de>
2022-08-13 00:30:43 +0200

    bumped some plugin metadata

--- src/build.gradle.kts
+++ src/build.gradle.kts
@@ -1,7 +1,7 @@
 import io.gitlab.arturbosch.detekt.Detekt
-import org.jetbrains.changelog.closure
 import org.jetbrains.changelog.markdownToHTML
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 
 fun properties(key: String) = project.findProperty(key).toString()
 
@@ -9,18 +9,23 @@ plugins {
     // Java support
     id("java")
     // Kotlin support
-    id("org.jetbrains.kotlin.jvm") version "1.4.32"
+    // do NOT update kotlin - kotlin version must match platform version, see https://plugins.jetbrains.com/docs/intellij/kotlin.html#kotlin-standard-library
+    id("org.jetbrains.kotlin.jvm") version "1.6.21"
     // gradle-intellij-plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
-    id("org.jetbrains.intellij") version "0.7.2"
+    id("org.jetbrains.intellij") version "1.8.0"
+    // id("com.jetbrains.rdgen") version "2022.2.5"
     // gradle-changelog-plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
-    id("org.jetbrains.changelog") version "1.1.2"
+    id("org.jetbrains.changelog") version "1.3.1"
     // detekt linter - read more: https://detekt.github.io/detekt/gradle.html
-    id("io.gitlab.arturbosch.detekt") version "1.16.0"
+    id("io.gitlab.arturbosch.detekt") version "1.21.0"
     // ktlint linter - read more: https://github.com/JLLeitschuh/ktlint-gradle
-    id("org.jlleitschuh.gradle.ktlint") version "10.0.0"
-    // parser & lexer (i.e. the bnf and the flex file...)
-    id("org.jetbrains.grammarkit") version "2020.3.2"
+    id("org.jlleitschuh.gradle.ktlint") version "10.3.0"
+    // grammarkit to generate parser & lexer (i.e. the bnf and the flex file...)
+    // ("org.jetbrains.grammarkit") version "2021.2.2"
 }
+
+val jvmVersion = "11"
+val kotlinVersion = "1.6" // should match org.jetbrains.kotlin.jvm (major.minor)
 
 group = properties("pluginGroup")
 version = properties("pluginVersion")
@@ -31,7 +36,7 @@ repositories {
     jcenter()
 }
 dependencies {
-    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.16.0")
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.21.0")
 }
 
 // for generated parser code..
@@ -40,21 +45,19 @@ sourceSets["main"].java.srcDirs("src/main/gen")
 // Configure gradle-intellij-plugin plugin.
 // Read more: https://github.com/JetBrains/gradle-intellij-plugin
 intellij {
-    pluginName = properties("pluginName")
-    version = properties("platformVersion")
-    type = properties("platformType")
-    downloadSources = properties("platformDownloadSources").toBoolean()
-    updateSinceUntilBuild = true
+    pluginName.set(properties("pluginName"))
+    version.set(properties("platformVersion"))
+    type.set(properties("platformType"))
+    downloadSources.set(properties("platformDownloadSources").toBoolean())
+    updateSinceUntilBuild.set(true)
 
     // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
-    setPlugins(*properties("platformPlugins").split(',').map(String::trim).filter(String::isNotEmpty).toTypedArray())
-}
-
-// Configure gradle-changelog-plugin plugin.
-// Read more: https://github.com/JetBrains/gradle-changelog-plugin
-changelog {
-    version = properties("pluginVersion")
-    groups = emptyList()
+    plugins.set(
+        properties("platformPlugins")
+            .split(',')
+            .map(String::trim)
+            .filter(String::isNotEmpty)
+    )
 }
 
 // Configure detekt plugin.
@@ -70,83 +73,95 @@ detekt {
     }
 }
 
-grammarKit {
-    // version of IntelliJ patched JFlex (see bintray link below), Default is 1.7.0-1
-    //jflexRelease = "1.7.0-1"
-
-    // tag or short commit hash of Grammar-Kit to use (see link below). Default is 2020.3.1
-    grammarKitRelease = "2020.1"
-}
-
 tasks {
-    register<org.jetbrains.grammarkit.tasks.GenerateLexer>("gen-lexer") {
-        dependsOn("gen-parser")
-        this.source = "src/main/java/com/github/nilsa/ideatestplugin/psi/Nils.flex"
-        this.targetDir = "src/main/gen/com/github/nilsa/ideatestplugin/psi/lex"
-        this.targetClass = "NilsLexer"
-        this.purgeOldFiles = true
-    }
-
-    register<org.jetbrains.grammarkit.tasks.GenerateParser>("gen-parser") {
-        this.source = "src/main/java/com/github/nilsa/ideatestplugin/psi/Nils.bnf"
-        this.targetRoot = "src/main/gen"
-        this.pathToParser = "/com/github/nilsa/ideatestplugin/psi/NilsParser.java"
-        this.pathToPsiRoot = "/com/github/nilsa/ideatestplugin/psi/parse"
-        this.purgeOldFiles = true
-    }
-
-    // Set the compatibility versions to 1.8
     withType<JavaCompile> {
-        sourceCompatibility = "1.8"
-        targetCompatibility = "1.8"
+        sourceCompatibility = jvmVersion
+        targetCompatibility = jvmVersion
     }
     withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "1.8"
-        dependsOn("gen-lexer", "gen-parser")
+        kotlinOptions {
+            jvmTarget = jvmVersion
+            languageVersion = kotlinVersion
+            apiVersion = kotlinVersion
+            freeCompilerArgs = listOf("-Xjvm-default=compatibility")
+        }
     }
-
     withType<Detekt> {
-        jvmTarget = "1.8"
+        jvmTarget = jvmVersion
+        excludes.add("**/gen/**")
+        reports {
+            html.required.set(false)
+            xml.required.set(false)
+            txt.required.set(false)
+        }
+    }
+    buildSearchableOptions {
+        enabled = false
+    }
+    // workaround for https://youtrack.jetbrains.com/issue/IDEA-210683
+    getByName<JavaExec>("buildSearchableOptions") {
+        jvmArgs(
+            // I gave up on tracking individual illegal access violations. 
+            // This seems to be an integral and unfixable part of IntelliJ.
+            "--illegal-access=permit"
+        )
+    }
+    withType<org.jlleitschuh.gradle.ktlint.tasks.KtLintCheckTask> {
+        exclude("**/gen/**", "**/*.Generated.kt")
     }
 
     patchPluginXml {
-        version(properties("pluginVersion"))
-        sinceBuild(properties("pluginSinceBuild"))
-        untilBuild(properties("pluginUntilBuild"))
+        version.set(properties("pluginVersion"))
+        sinceBuild.set(properties("pluginSinceBuild"))
+        untilBuild.set(properties("pluginUntilBuild"))
 
         // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
-        pluginDescription(
-            closure {
-                File(projectDir, "README.md").readText().lines().run {
+        pluginDescription.set(
+            provider {
+                File(projectDir, "./plugin_description.md").readText().lines().run {
                     val start = "<!-- Plugin description -->"
                     val end = "<!-- Plugin description end -->"
 
                     if (!containsAll(listOf(start, end))) {
-                        throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
+                        throw GradleException(
+                            "Plugin description section not found in plugin_description.md:\n$start ... $end"
+                        )
                     }
                     subList(indexOf(start) + 1, indexOf(end))
                 }.joinToString("\n").run { markdownToHTML(this) }
             }
         )
 
-        // Get the latest available change notes from the changelog file
-        changeNotes(
-            closure {
-                changelog.getLatest().toHTML()
+        changeNotes.set(
+            provider {
+                File(projectDir, "./plugin_description.md").readText().lines().run {
+                    val start = "<!-- Plugin changeNotes -->"
+                    val end = "<!-- Plugin changeNotes end -->"
+
+                    if (!containsAll(listOf(start, end))) {
+                        throw GradleException(
+                            "Plugin changeNotes section not found in plugin_description.md:\n$start ... $end"
+                        )
+                    }
+                    subList(indexOf(start) + 1, indexOf(end))
+                }.joinToString("\n").run { markdownToHTML(this) }
             }
         )
     }
 
     runPluginVerifier {
-        ideVersions(properties("pluginVerifierIdeVersions"))
+        // verifierVersion.set("1.256") // default ist "latest".
+        ideVersions.addAll(
+            properties("pluginVerifierIdeVersions")
+                .split(',')
+                .map(String::trim)
+                .filter(String::isNotEmpty)
+        )
+        // reports are in ${project.buildDir}/reports/pluginVerifier - or set verificationReportsDirectory()
     }
 
     publishPlugin {
-        dependsOn("patchChangelog")
-        token(System.getenv("PUBLISH_TOKEN"))
-        // pluginVersion is based on the SemVer (https://semver.org) and supports pre-release labels, like 2.1.7-alpha.3
-        // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
-        // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
-        channels(properties("pluginVersion").split('-').getOrElse(1) { "default" }.split('.').first())
+        token.set(System.getenv("PUBLISH_TOKEN"))
+        channels.set(listOf(properties("marketplaceChannel")))
     }
 }
---
+++ src/plugin_description.md
@@ -0,0 +1,16 @@
+# Plugin description
+
+This file is read during build and for building the description
+and the change-notes for the plugin.
+The reason this file is here, is because it is much easier to write the documentation
+in markdown, than to write the xml-encoded html into `plugin.xml`.
+
+## The Description
+<!-- Plugin description -->
+My cool plugin. It's really cool!
+<!-- Plugin description end -->
+
+## Change-Notes
+<!-- Plugin changeNotes -->
+None, so far.
+<!-- Plugin changeNotes end -->
